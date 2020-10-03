fe9121d88354e8b227b6d5318a46da7fb88f6787
Roseline Shapi <roseshapi81@gmail.com>
2020-10-02 23:21:43 -0500

    fixed collection View layout

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Roseline Shapi on 10/2/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
